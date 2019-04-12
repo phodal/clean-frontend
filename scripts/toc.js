9dc545696ac5ff88720c2d182737a235652a9f13
Phodal HUANG <h@phodal.com>
2019-04-12 10:03:18 +0800

    build: update scripts path

--- scripts/toc.js
+++ scripts/toc.js
@@ -4,8 +4,10 @@ const path = require("path");
 const execa = require("execa");
 const fs = require("fs");
 
+const tools = path.resolve("tools");
 const docs = path.resolve("docs");
 const readme = path.resolve(docs, "README.md");
+
 execa
-  .stdout(path.resolve(docs, "generate-toc.sh"))
+  .stdout(path.resolve(tools, "generate-toc.sh"))
   .then(toc => fs.writeFileSync(readme, toc, "utf8"));
