0f4d826de9431ff1f47d14810f7c19df85313bdd
Phodal HUANG <h@phodal.com>
2019-04-12 09:34:39 +0800

    fix: fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavBarModule } from './shared/components/navbar/navbar.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        NavBarModule,
         RouterTestingModule
       ],
       declarations: [
--- src/app/shared/components/navbar/navbar.component.spec.ts
+++ src/app/shared/components/navbar/navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { ThemePickerModule } from '../../utils/theme-picker';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -8,6 +10,10 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ThemePickerModule,
+        RouterTestingModule
+      ],
       declarations: [ NavbarComponent ]
     })
     .compileComponents();
