8bb0233e3459e6e22d17479af0ff4eed2b7ba5f8
Phodal HUANG <h@phodal.com>
2019-07-17 20:08:37 +0800

    fix: fix tests

--- src/app/shared/components/clean-navbar/clean-navbar.component.spec.ts
+++ src/app/shared/components/clean-navbar/clean-navbar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CleanNavbarComponent } from './clean-navbar.component';
+import { ThemePickerModule } from '../../utils/theme-picker';
 
 describe('CleanNavbarComponent', () => {
   let component: CleanNavbarComponent;
@@ -8,6 +10,10 @@ describe('CleanNavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ThemePickerModule,
+        RouterTestingModule
+      ],
       declarations: [ CleanNavbarComponent ]
     })
     .compileComponents();
