7ed3191d20e2fb93cb4b44d46fe63b3388b8dc17
Phodal HUANG <h@phodal.com>
2019-07-17 20:03:19 +0800

    build: rename directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: '/home'},
   {
     path: 'home',
-    loadChildren: './public/home/home.module#HomeModule'
+    loadChildren: './presentation/home/home.module#HomeModule'
   }
 ];
 
